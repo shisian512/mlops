600d20bba713097262390905a79890582b44f4f7
shisiantan <shisian001@gmail.com>
2025-07-17 14:26:42 +0800

    champion challenger test implementation (WIP)

--- train.py
+++ train.py
@@ -4,6 +4,8 @@ from sklearn.model_selection import train_test_split
 import pandas as pd
 import yaml, mlflow, mlflow.sklearn, joblib, json
 from mlflow import MlflowClient
+# import warnings
+# warnings.simplefilter(action='ignore', category=FutureWarning)
 
 # Use Mlflow autologging to automatically log parameters, metrics, and models
 # Set experiment name to avoid 'experiment ID 0 not found' error
@@ -26,8 +28,40 @@ model_name = "regression_model"
 alias = "challenger"
 
 
-def run_comparison():
-    return True
+# def run_comparison():
+#     return True
+
+def run_comparison(model_name, metric_key="mse_custom"):
+    client = MlflowClient()
+    
+    # get champion model version and its custom metric
+    # try:
+    champ_mv = client.get_model_version_by_alias(model_name, "champion")
+    champ_run_id = champ_mv.run_id
+    print("DEBUG champ_run_id : ", champ_run_id)
+    champ_metrics = client.get_metric_history(champ_run_id, metric_key)
+    champ_value = champ_metrics[-1].value if champ_metrics else float("inf")
+    print(f"Champion v{champ_mv.version} {metric_key} = {champ_value:.6f}")
+    # except Exception:
+    #     champ_mv = None
+    #     champ_value = float("inf")
+    #     print("No Champion, take challenger.")
+    
+    # get challenger model version and its custom metric
+    chall_mv = client.get_model_version_by_alias(model_name, "challenger")
+    chall_run_id = chall_mv.run_id
+    print("DEBUG chall_run_id : ", chall_run_id)
+    chall_metrics = client.get_metric_history(chall_run_id, metric_key)
+    chall_value = chall_metrics[-1].value if chall_metrics else float("inf")
+    print(f"Challenger v{chall_mv.version} {metric_key} = {chall_value:.6f}")
+    
+    # comparison: the lower the better
+    if chall_value < champ_value:
+        print("Challenger better, should promote it to Champion.")
+        return True
+    else:
+        print("Challenger is no better than Champion.")
+        return False
 
 
 # Train the model and log parameters, metrics, and model artifacts using MLflow
@@ -67,7 +101,7 @@ with mlflow.start_run():
     )
     
     # Run comparison of challenger and champion models
-    current_model_is_better = run_comparison() # either return true or false
+    current_model_is_better = run_comparison(model_name) # either return true or false
     
     # If challenger is better, it will be registered as the champion
     if current_model_is_better:
@@ -83,4 +117,9 @@ with mlflow.start_run():
     # # Print the model information of latest champion (deployed in production)
     # model_version_alias = "champion"
     # model_info = client.get_model_version_by_alias(model_name, model_version_alias)
-    # print("Model Info for Champion model : ", model_info)
+    # run_id = model_info.run_id
+    # print("Model Info for Champion model : ", run_id)
+    # champ_metrics = client.get_metric_history(run_id, "mse_custom")
+    # champ_value = champ_metrics[-1].value if champ_metrics else float("inf")
+    # print("DEBUG : champ_value ", champ_value)
+    # print(f"Champion v{model_info.version} mse_custom = {champ_value:.6f}")
