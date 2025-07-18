452a109b48c2549bd7fbb8567752a35adaaad370
shisiantan <shisian001@gmail.com>
2025-07-18 16:47:00 +0800

    refactor: reorganize project structure and update Docker configurations for improved modularity

--- src/models/predict.py
+++ src/models/predict.py
@@ -1,9 +1,7 @@
 import mlflow
 import mlflow.sklearn
 
-# set MLflow tracking
 mlflow.set_tracking_uri("http://mlflow:5000")
-# mlflow.set_tracking_uri("http://192.168.0.124:5000")
 model_name = "sk-learn-random-forest-reg-model@production"
 model_uri = f"models:/{model_name}"
 model = mlflow.sklearn.load_model(model_uri)
