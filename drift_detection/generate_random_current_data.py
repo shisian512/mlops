f3b2b165f08a6f0137452fcc38fbba8c55659483
shisiantan <shisian001@gmail.com>
2025-07-19 21:27:20 +0800

    test data drift

---
+++ drift_detection/generate_random_current_data.py
@@ -0,0 +1,22 @@
+import pandas as pd
+import numpy as np
+
+df = pd.read_csv('/home/user/mlops/drift_detection/train.csv')
+
+df_fake = pd.DataFrame()
+
+for col in df.columns:
+    if pd.api.types.is_numeric_dtype(df[col]):
+        mean = df[col].mean()
+        std = df[col].std()
+        df_fake[col] = np.random.normal(loc=mean, scale=std, size=len(df))
+    elif pd.api.types.is_bool_dtype(df[col]):
+        df_fake[col] = np.random.choice([True, False], size=len(df))
+    elif pd.api.types.is_categorical_dtype(df[col]) or df[col].dtype == object:
+        df_fake[col] = np.random.choice(df[col].dropna().unique(), size=len(df))
+    else:
+        df_fake[col] = df[col]
+
+df_fake.to_csv("current.csv", index=False)
+
+print("✅ Generated file: current.csv")
