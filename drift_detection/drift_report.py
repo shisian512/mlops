d6cfe45e10dadaa2919d1cdd94495fbfaa0cc46a
shisiantan <shisian001@gmail.com>
2025-07-19 21:15:19 +0800

    implemented data drift detection

--- drift_detection/drift_report.py
+++ drift_detection/drift_report.py
@@ -1,19 +1,13 @@
-from evidently import Report
-from evidently.metrics import DataDriftPreset
+from evidently.report import Report
+from evidently.metric_preset import DataDriftPreset
 import pandas as pd
 import os
 
 # Example data loading
-ref = pd.read_csv("train.csv")
-cur = pd.read_csv("current.csv")
+ref = pd.read_csv("../data/train.csv")
+cur = pd.read_csv("../data/current.csv")
 
 # Generate report
 report = Report(metrics=[DataDriftPreset()])
 report.run(reference_data=ref, current_data=cur)
-
-# # Create reports directory if it doesn't exist
-# os.makedirs("reports", exist_ok=True)
-
-# Save the report
-report.save_html("full_drift_report.html")
-report.save_json("full_drift_report.json")
+report.save_html("report.html")
